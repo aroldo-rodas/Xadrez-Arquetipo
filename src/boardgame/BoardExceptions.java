43d16c5fdae38de657b6ad6132977ee9122ae625
Aroldo <aroldobusiness@hotmail.com>
2024-08-07 21:07:18 -0400

    Programacao defensiva

---
+++ src/boardgame/BoardExceptions.java
@@ -0,0 +1,7 @@
+package boardgame;
+
+public class BoardExceptions extends RuntimeException {
+    public BoardExceptions(String msg) {
+        super(msg);
+    }
+}
--- src/boardgame/Tabuleiro.java
+++ src/boardgame/Tabuleiro.java
@@ -8,6 +8,9 @@ public class Tabuleiro {
 
     //Construtor
     public Tabuleiro(int linhas, int colunas) {
+        if(linhas < 1 || colunas < 1) {
+            throw new BoardExceptions("Erro criando tabuleiro");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
@@ -22,29 +25,41 @@ public class Tabuleiro {
         return colunas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
-
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-
     //Métodos
     public Peca peca(int linha, int coluna) {
+        if(!posicaoExite(linha, coluna)) {
+            throw new BoardExceptions("Posicao nao existe!");
+        }
         return pecas[linha][coluna];
     }
     
     public Peca peca(Posicao posicao) {
+        if(!posicaoExite(posicao)) {
+            throw new BoardExceptions("Posicao nao existe!");
+        }
         return pecas[posicao.getLinha()][posicao.getColuna()];
     }
 
     public void posicaoPeca(Peca peca, Posicao posicao) {
+        if(existeUmaPeca(posicao)) {
+            throw new BoardExceptions("Existe uma peca na posição!");
+        }
         pecas[posicao.getLinha()][posicao.getColuna()] = peca;
         peca.posicao = posicao;
     }
 
-    
+    private boolean posicaoExite(int linha, int coluna) {
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
 
+    public boolean posicaoExite(Posicao posicao) {
+        return posicaoExite(posicao.getLinha(), posicao.getColuna());
+    }
     
+    public boolean existeUmaPeca(Posicao posicao) {
+        if(!posicaoExite(posicao)) {
+            throw new BoardExceptions("Posicao nao existe!");
+        }
+        return (peca(posicao) != null);
+    }
 }
